2cd30eddf7471c9f2a7db8e7a8da1325b791daec
Logan Lucas <logs10658@gmail.com>
2024-09-14 00:43:26 -0500

    changed theme of website to be more modern; entire project revamped

---
+++ main.js
@@ -0,0 +1,5 @@
+// changes the visibilty of the settings menu upon clicking the button in the header
+function toggleSettings() {
+    const hiddenMenu = document.querySelector('.hiddenMenu');
+    hiddenMenu.classList.toggle('active');
+}
